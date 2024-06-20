c1830aadee8498bf3e2c90c7c332b6fc5954c487
ssprasad-23 <shynal_007@yahoo.com>
2024-06-20 15:36:18 -0700

    implemented geolocation feature

--- Weather.tsx
+++ Weather.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState} from "react";
 import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 import { API_KEY } from "@env";
+import Geolocation from '@react-native-community/geolocation';
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.787419&lon=-122.448586&appid=${API_KEY}&units=imperial`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
 
 type Weather = {
     name: string;
@@ -18,25 +19,30 @@ type Weather = {
     };
 };
 
-
 const Home = () => {
-    const [weather, setWeather] = useState<Weather>();
-    
-    const fetchWeather = async () => {
-        //fetch data from api
-        const results = await fetch (url);
-        const data = await results.json();
-        console.log(JSON.stringify(data, null, 2));
-        setWeather(data);
-    }    
+  const [weather, setWeather] = useState<Weather>();
 
-    useEffect(() => {
-        fetchWeather();
-    }, []);   
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+        async (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log(latitude, longitude);
 
-    if (!weather) {
-        return <ActivityIndicator />;
-    }
+            const results = await fetch(
+                `${BASE_URL}?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+            );
+            const data = await results.json();
+            console.log(JSON.stringify(data, null, 2));
+            setWeather(data);
+        },
+        (error) => console.log(error.message),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+
+  if (!weather) {
+    return <ActivityIndicator />;
+  }
     
   return (
     <View style={styles.container}>
